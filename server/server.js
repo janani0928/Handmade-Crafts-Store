03794c3bc7c875b71ef8a1eb9ffd15d3fb8510d8
Janani M <jananim2809@gmaill.com>
2025-12-29 10:41:11 +0530

    one

--- server/server.js
+++ server/server.js
@@ -19,8 +19,9 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://handmade-crafts-store-1.onrender.com"
+  "https://handmade-crafts-store.netlify.app"
 ];
+
 
 app.use(
   cors({
