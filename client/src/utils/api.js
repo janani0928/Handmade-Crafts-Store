226afec93fce730139026f33c9c09b0316162667
Janani M <jananim2809@gmaill.com>
2025-12-29 10:59:05 +0530

    one

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,2 @@
-const API_BASE_URL =
-  import.meta.env.PROD
-    ? "https://handmade-crafts-store-1.onrender.com/api"
-    : "http://localhost:5000/api";
-
+const API_BASE_URL = "/api";
 export default API_BASE_URL;
